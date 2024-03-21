388cc8a0b193a66fe52c0b95a169edde7165e257
Debbie012 <ntigelidebbie@gmail.com>
2024-03-21 21:43:31 +0100

    added Task 14

---
+++ Task 14/src/Main.java
@@ -0,0 +1,16 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello and welcome to my page. This is my first Java Project");
+        System.out.println("Let's learn Java Programming!");
+        System.out.println("Java is a powerful and flexible programming language.");
+        System.out.println("I am excited to explore more Java concepts and projects.");
+        System.out.println("Happy coding!");
+
+        //for (int i = 1; i <= 5; i++) {
+       //     System.out.println("i = " + i);
+        }
+    }
+//}
